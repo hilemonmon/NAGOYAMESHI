ebb8196467af99ecb8863b0847d6588421dbcad0
S.Endo <a12.5hjn@gmail.com>
2025-06-16 19:11:22 +0900

    signup URLの末尾スラッシュを削除

--- src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
+++ src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig {
                                 "/js/**",
                                 "/storage/**",
                                 "/",
-                                "/signup/",
+                                "/signup",
                                 "/signup/**",
                                 "/register",
                                 "/verify"
